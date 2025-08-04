c42e8273c4e97c38803bde98627c247c210a447b
tianping526 <1553260215@qq.com>
2025-08-04 17:25:44 +0800

    feat(auth): Optionally, you can provide an auth key to enable the auth feature

--- app/service/internal/server/grpc.go
+++ app/service/internal/server/grpc.go
@@ -3,12 +3,15 @@ package server
 import (
 	validateV2 "github.com/go-kratos/kratos/contrib/middleware/validate/v2"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/metrics"
 	"github.com/go-kratos/kratos/v2/middleware/ratelimit"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/middleware/tracing"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
+	jwtV5 "github.com/golang-jwt/jwt/v5"
 	"go.opentelemetry.io/otel/trace"
 
 	v1 "github.com/tianping526/eventbridge/apis/api/eventbridge/service/v1"
@@ -26,22 +29,27 @@ func NewGRPCServer(
 	_ trace.TracerProvider, // otel.SetTracerProvider(provider) instead, but need to declare to wire injection
 ) *grpc.Server {
 	cs := bc.Server
-	// ca := bc.Auth
+	ca := bc.Auth
+	middlewares := []middleware.Middleware{
+		recovery.Recovery(),
+		tracing.Server(),
+		metrics.Server(
+			metrics.WithSeconds(m.DurationSec),
+			metrics.WithRequests(m.CodeTotal),
+		),
+		recovery.Recovery(),
+		logging.Server(logger),
+		ratelimit.Server(),
+		validateV2.ProtoValidate(),
+	}
+	if ca != nil && ca.Key != "" {
+		middlewares = append(middlewares, jwt.Server(func(_ *jwtV5.Token) (interface{}, error) {
+			return []byte(ca.Key), nil
+		}))
+	}
 	opts := []grpc.ServerOption{
 		grpc.Middleware(
-			recovery.Recovery(),
-			tracing.Server(),
-			metrics.Server(
-				metrics.WithSeconds(m.DurationSec),
-				metrics.WithRequests(m.CodeTotal),
-			),
-			recovery.Recovery(),
-			logging.Server(logger),
-			ratelimit.Server(),
-			validateV2.ProtoValidate(),
-			// jwt.Server(func(token *jwtV4.Token) (interface{}, error) {
-			//	return []byte(ca.Key), nil
-			// }),
+			middlewares...,
 		),
 	}
 	if cs.Grpc.Network != "" {
