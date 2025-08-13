2423160b2c5b3b15a5468fa6c9054c9585111226
link.tian <link.tian@quantphemes.com>
2025-08-13 12:37:33 +0800

    chore(deps): upgrade Go version to 1.25.0 and update dependencies

--- app/job/generate.go
+++ app/job/generate.go
@@ -1,6 +1,6 @@
 package generate
 
-//go:generate go tool buf generate --template {"version":"v2","plugins":[{"remote":"buf.build/protocolbuffers/go:v1.34.2","out":".","opt":["paths=source_relative"]}]}
+//go:generate go tool buf generate --template {"version":"v2","plugins":[{"local":["go","run","google.golang.org/protobuf/cmd/protoc-gen-go"],"out":".","opt":["paths=source_relative"]}]}
 //go:generate go tool ent generate ./internal/data/ent/schema --feature sql/lock
 //go:generate go tool wire ./cmd/server
 //go:generate go tool wire ./test
